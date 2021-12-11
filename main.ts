6d4f0cb9fbf0a35136bde00cab2ef71753b024ee
EdifyRobotics <ysy_0311@hotmail.com>
2021-12-11 12:41:12 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -236,8 +236,8 @@ namespace CodeRorver {
 			            // left side counter-clockwise
 			            pins.analogWritePin(AnalogPin.P1, Math.round(1023 * fasterSpeed / 100))
 			            pins.digitalWritePin(DigitalPin.P15, 0)
-			            pins.digitalWritePin(DigitalPin.P3, 0)
-			            pins.digitalWritePin(DigitalPin.P9, 1)
+			            // pins.digitalWritePin(DigitalPin.P3, 0)
+			            // pins.digitalWritePin(DigitalPin.P9, 1)
 			        } else if (hall1Count > hall2Count) {
 			            // right side slower
 			            fasterSpeed = initialSpeed + pValue * (hall1Count - hall2Count)
@@ -256,8 +256,8 @@ namespace CodeRorver {
 			            // left side counter-clockwise
 			            pins.analogWritePin(AnalogPin.P1, Math.round(1023 * slowerSpeed / 100))
 			            pins.digitalWritePin(DigitalPin.P15, 0)
-			            pins.digitalWritePin(DigitalPin.P3, 1)
-			            pins.digitalWritePin(DigitalPin.P9, 1)
+			            // pins.digitalWritePin(DigitalPin.P3, 1)
+			            // pins.digitalWritePin(DigitalPin.P9, 1)
 			        } else {
 			            // p0p6 is the same as p1p7
 			            pins.analogWritePin(AnalogPin.P0, 1023 - Math.round(1023 * initialSpeed / 100))
@@ -265,8 +265,8 @@ namespace CodeRorver {
 			            // left side counter-clockwise
 			            pins.analogWritePin(AnalogPin.P1, Math.round(1023 * initialSpeed / 100))
 			            pins.digitalWritePin(DigitalPin.P15, 0)
-			            pins.digitalWritePin(DigitalPin.P3, 0)
-			            pins.digitalWritePin(DigitalPin.P9, 0)
+			            // pins.digitalWritePin(DigitalPin.P3, 0)
+			            // pins.digitalWritePin(DigitalPin.P9, 0)
 			        }
 		       	}
 		       	//move backward straight
