fd1dae64324380e801ca09427cffef88fd40c431
EdifyRobotics <ysy_0311@hotmail.com>
2021-12-14 22:59:24 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -397,7 +397,7 @@ namespace CodeRorver {
 		       	}
 		       	//move backward straight
 		       	else if(direction==CodeRoverDriveDirection.Backward){
-		       		if (hall1Count < hall2Count) {
+		       		if (hall1Count > hall2Count) {
 			            fasterSpeed = initialSpeed + pValue * (hall2Count - hall1Count)
 			            slowerSpeed = initialSpeed - pValue * (hall2Count - hall1Count)
 			            if (fasterSpeed > 70) { //limit max speed to 70% and min speed to 10%
@@ -416,9 +416,12 @@ namespace CodeRorver {
 			            // p0p6 is faster than p1p7, p0p6 slow down
 			            pins.analogWritePin(AnalogPin.P1, 1023 - Math.round(1023 * fasterSpeed / 100))
 			            pins.digitalWritePin(DigitalPin.P15, 1)
+
+
+
 			            // pins.digitalWritePin(DigitalPin.P3, 0)
 			            // pins.digitalWritePin(DigitalPin.P9, 1)
-			        } else if (hall1Count > hall2Count) {
+			        } else if (hall1Count < hall2Count) {
 			            // right side slower
 			            fasterSpeed = initialSpeed + pValue * (hall1Count - hall2Count)
 			            slowerSpeed = initialSpeed - pValue * (hall1Count - hall2Count)
